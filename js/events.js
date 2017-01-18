617cdfd1e0d005d541973c5615442d08d24c4e5f
davidkorff <davidkorff@gmail.com>
2017-01-18 15:31:19 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt () {
+  $('img').on("load", function() {
+    this.className = 'tasty'
+  })
+}
+
+function pressIt () {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71){
+      alert('Yo boy!!! You pressed g!');
+    }
+  })
+}
+
+function submitIt () {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+    })
+}
 
 $(document).ready(function(){
 
